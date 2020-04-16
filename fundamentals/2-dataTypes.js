90085203dae230ce87233a0489cb6a1f6971194d
Mel1207 <meljohnpualon@gmail.com>
2020-04-17 02:22:50 +0800

    added new lessons

---
+++ fundamentals/2-dataTypes.js
@@ -0,0 +1,43 @@
+// PRIMITIVE DATA TYPES
+// STRING 
+
+const car = 'honda fit';
+console.log(typeof car);
+
+// NUMBER
+
+const age = 25;
+console.log(typeof age);
+
+// BOOLEAN
+
+const isOlder = true;
+console.log(typeof isOlder);
+
+// NULL
+
+const house = null;
+console.log(typeof house);
+
+// SYMBOL - ES6
+const sym = Symbol();
+console.log(typeof sym);
+
+
+// REFFERENCE DATA TYPES
+// OBJECT LITERALS
+const laptop = {
+    brand: 'ASUS-X311',
+    cpu: 'Intel Core i7'
+}
+
+// ARAYS
+const favoriteFruits = ['manggo', 'apple', 'pears'];
+
+// FUNCTIONS
+const greetings = function () {
+    return `Hello There`;
+}
+
+// DATE
+const today = new Date();
