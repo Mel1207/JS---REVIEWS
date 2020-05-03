1361cdedc1146072f2a12b4e73ee908eba8fdcb8
Mel1207 <meljohnpualon@gmail.com>
2020-05-03 21:44:48 +0800

    added new lesson Public API's

---
+++ AJAX - Lessons/FetchingDATA-PublicAPI/app.js
@@ -0,0 +1,37 @@
+document.querySelector('.btn-info').addEventListener('click', getJokes);
+
+function getJokes (e) {
+    const number = document.querySelector('#number').value;
+    // console.log(number);
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function () {
+        if(this.status === 200) {
+            const response = JSON.parse(this.responseText);
+
+            let output = '';
+
+            if(response.type === 'success') {
+                response.value.forEach(function (joke) {
+                    output += `<li class="list-group-item">${joke.joke}</li>`
+                });
+            } else {
+                output += `<li class="list-group-item">Something went wrong</li>`
+            }
+
+            document.querySelector('.Jokes').innerHTML = output;
+            console.log(response);
+        }
+    }
+
+
+    xhr.send();
+    // Trial of function
+    // console.log('get jokes');
+
+
+    e.preventDefault();
+}
