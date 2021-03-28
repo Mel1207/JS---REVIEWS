a38c59606755deb005baf011c7c30f7e74e46061
Mel1207 <meljohnpualon@gmail.com>
2021-03-28 13:55:15 +0800

    added new lesson number data types

--- JS-CC21/numbers.js
+++ JS-CC21/numbers.js
@@ -17,4 +17,21 @@ const pi = 3.14;
 
 // oder of operation - BIDMAS
 
+// let result = 5 * (10-3)**2;
+
+let likes = 10;
+// like = 10 + 1;
+// or
+// likes++;
+// likes--;
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+// NAN - not a number
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = 'the blog has ' + likes + ' likes';
 console.log(result)
