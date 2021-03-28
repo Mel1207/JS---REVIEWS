4c41627b6a49337db583d08b33c8675e3b7fffbe
Mel1207 <meljohnpualon@gmail.com>
2021-03-28 19:39:24 +0800

    added booleans and type conversion

---
+++ JS-CC21/booleans.js
@@ -0,0 +1,46 @@
+// Booleans and comparisons
+// console.log(true, false, "true", "false")
+
+
+// methods can return booleans
+let email = 'mel@gmail.com'
+let names = ['Mel', 'Juvy', 'John']
+// let determineEmail = email.includes('@');
+// let determineName = names.includes('Juvy')
+
+// console.log(determineName)
+// console.log(determineEmail)
+
+// comparison operators
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age != 25);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'Mel';
+
+// console.log(name == 'Mel');
+// console.log(name == 'mel');
+// console.log(name > 'Juvy');
+// console.log(name > 'mel');
+// console.log(name < 'Juvy');
+
+let age = 25;
+// loose comparison (different types can still be equal)
+// console.log(age == 25)
+// console.log(age == '25')
+// console.log(age != 25);
+// console.log(age != '25');
+
+// Strict comparison (different types cannot be equal)
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
+
---
+++ JS-CC21/typeconversion.js
@@ -0,0 +1,16 @@
+// type conversion
+
+let score = '100'
+
+score = Number(score);
+console.log(score + 1);
+
+// checking the type of variable;
+// console.log(typeof score);
+
+// let result = Number('whohoo');
+// let result = String(50); 
+// let result = Boolean(0);
+// let result = Boolean('0');
+
+console.log(result, typeof result);
