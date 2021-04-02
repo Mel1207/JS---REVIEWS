a449a4deaba006309a7ba7496a09034fddbbea4e
Mel <meljohnpualon@gmail.com>
2021-04-03 02:23:40 +0800

    finished var and block scopes lesson

--- JS-CC21/control-flow/var-and-block-scope.js
+++ JS-CC21/control-flow/var-and-block-scope.js
@@ -6,6 +6,11 @@ if (true) {
     age = 40; /* this is now a local scope variable - this scope can only accessed inside of this block code*/
     let name = 'Mel'
     console.log('inside 1st code block: ', age, name)
+
+    if(true) {
+        let age  = 50;
+        console.log('inside 2nd code block', age); /* this will get the value of the nearest variable inside the code block - means this will prefer the local */
+    }
 }
 
-console.log('outside code block: ', age, name)
+console.log('outside code block: ', age, name);
