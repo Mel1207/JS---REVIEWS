8e8c2c5128ba4c0f98a8ec0b612e08c97b38eb2f
Mel <meljohnpualon@gmail.com>
2021-04-05 00:02:04 +0800

    added math object lessons

--- JS-CC21/objects/mathObject.js
+++ JS-CC21/objects/mathObject.js
@@ -1,1 +1,16 @@
 // Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+console.log(Math.round(area)); // round the integer to the nearest
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area)); // take away the decimal and leave the integer
+
+// random numbers
+const random = Math.random()
+console.log(random)
+console.log(Math.round(random * 100))
