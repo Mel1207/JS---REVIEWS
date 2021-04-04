012ecf19088a91c45737863dca5ad957e62c3c14
Mel <meljohnpualon@gmail.com>
2021-04-04 20:51:44 +0800

    added functions and methods comparison

---
+++ JS-CC21/Functions/functions&Methods.js
@@ -0,0 +1,14 @@
+const firstName = 'Mel';
+
+// Functions
+const greet = () => 'Hi there!'
+let result = greet ();
+
+console.log(result)
+
+
+// Methods
+let resultTwo = firstName.toUpperCase();
+console.log(resultTwo);
+
+// Note: Functions and methods are similar but different on where we invoke them, methods are just a functions but can only be invoked by (.) dot notation, this methods are already inside the browser. but we can create our own method too.
