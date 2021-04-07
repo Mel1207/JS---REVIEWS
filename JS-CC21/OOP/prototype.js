0446fdda1782bf3b75034934387477ec62862f8e
Mel <meljohnpualon@gmail.com>
2021-04-07 22:32:21 +0800

    added prototype lesson

---
+++ JS-CC21/OOP/prototype.js
@@ -0,0 +1,28 @@
+// Create a prototype
+
+function People (Name, Age, Gender) {
+    this.Name = Name;
+    this.Age = Age;
+    this.Gender = Gender;
+
+    // this.getInfo = function () { // the function is called a method
+    //     return `Hi my name is ${this.Name} im ${this.Age} y.o ${this.Gender}`
+    // }
+}
+
+// this is now a prototype - Note: this will behave as a function inside an object
+People.prototype.getInfo = function () {
+    return `Hi my name is ${this.Name} im ${this.Age} y.o ${this.Gender}`
+}
+
+// Practice new creation of prototype
+People.prototype.getAge = function () {
+    return `${this.Name} is now ${this.Age} Y.O`
+}
+
+const people = new People ('Mel', 25, 'Male');
+const people2 = new People ('Juvy', 25, 'Female');
+
+
+console.log(people.getAge());
+
