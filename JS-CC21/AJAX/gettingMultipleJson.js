e20fa8f1443f78b57f4f21147bf813d640f30e5d
Mel <meljohnpualon@gmail.com>
2021-04-10 21:51:36 +0800

    added multiple fetch json and nested callbacks

--- JS-CC21/AJAX/gettingMultipleJson.js
+++ JS-CC21/AJAX/gettingMultipleJson.js
@@ -1,4 +1,4 @@
-const getTodos = (callBack) => {
+const getTodos = (resource, callBack) => {
     
     const request = new XMLHttpRequest();
 
@@ -12,15 +12,38 @@ const getTodos = (callBack) => {
         }
     });
 
-    request.open('GET', 'todos.json');
+    request.open('GET', resource);
     request.send();
 }
 
-getTodos((error, data) => {
-    console.log('callback fired');
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(data);
-    }
+
+// this is called callback worst(he.l), it means nag nested tayo ng maraming callback sa loob ng call back, ayos lang naman ito, pero mahirap imaintain kapag masyadong marami
+// const nito mahirap basahin, and pwede rin bumagal ang system dahil loaded yung callback natin ng isa pang callback etc. kaya dito papasok si promises
+// const pa nito, need nyang matapos muna mag fetch yung unang callback bago sumunod yung isang callback
+// getTodos('mel.json', (error, data) => {
+//     console.log(data);
+//     getTodos('juvy.json', (error, data) => {
+//         console.log(data);
+//         getTodos('john.json', (error, data) => {
+//             console.log(data);
+//         })
+//     });
+// });
+
+getTodos('mel.json', (error, data) => {
+    console.log(data);
+    
+    // example scenario if maraming load or mabigat yung data na need iload
+    setTimeout(() => {
+        getTodos('juvy.json', (error, data) => {
+            console.log(data);
+            setTimeout(() => {
+                getTodos('john.json', (error, data) => {
+                    console.log(data);
+                })
+            }, 3000)
+        });
+    }, 3000);
 });
+
+// one solution for this is promises
