975531a18a9a59dbb6e0f726295ddc2851f8407c
Mel <meljohnpualon@gmail.com>
2021-04-11 13:30:55 +0800

    updated custom error.js

--- JS-CC21/AJAX/creatingCustomError.js
+++ JS-CC21/AJAX/creatingCustomError.js
@@ -3,13 +3,10 @@
 
 const getTodos = async() => {
     const response = await fetch('mel.json');
-
     if(response.status !== 200) {
         throw new Error('Cannot Fetch the data');
     }
-
     const data = await response.json();
-
     return data;
 };
 
