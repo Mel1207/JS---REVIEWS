5e0cd3dcc203cf59aeb6e5b3e20c1b1f8503f62e
Mel <meljohnpualon@gmail.com>
2021-08-11 22:46:59 +0800

    added event delegation process

--- vanilaJSnav/app.js
+++ vanilaJSnav/app.js
@@ -1,15 +1,32 @@
 'use strict'
 
 const navlinkAll = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelector('.nav-links');
 
-navlinkAll.forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault()
+// old and inneficient syntax
+// navlinkAll.forEach(link => {
+//     link.addEventListener('click', e => {
+//         e.preventDefault()
 
-        const id = link.getAttribute('href');
+//         const id = link.getAttribute('href');
+
+//         document.querySelector(id).scrollIntoView({
+//             behavior: "smooth"
+//         })
+//     })
+// })
+
+// event delegation process
+navLinks.addEventListener('click', e => {
+    e.preventDefault();
+
+    // element match making or validating
+    if(e.target.classList.contains('nav-link')) {
+        const id = e.target.getAttribute('href');
+        console.log(id)
 
         document.querySelector(id).scrollIntoView({
             behavior: "smooth"
         })
-    })
+    }
 })
