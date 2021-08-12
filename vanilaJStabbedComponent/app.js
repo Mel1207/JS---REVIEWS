2e0606ceaf12973c6b54fe9b8bd09073c37fdaeb
Mel <meljohnpualon@gmail.com>
2021-08-13 05:44:11 +0800

    added element vars and fix some ui on tab text

--- vanilaJStabbedComponent/app.js
+++ vanilaJStabbedComponent/app.js
@@ -1,2 +1,5 @@
 'use strict'
 
+const tabBtn = document.querySelectorAll('.tab-item-btn');
+const tabBtnContainer = document.querySelector('.tabbed-items-container');
+const tabContent = document.querySelectorAll('.tab-items-content');
