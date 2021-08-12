f48aabec1dc98b7e8ed973304415b2e06b4d8f57
Mel <meljohnpualon@gmail.com>
2021-08-13 06:18:14 +0800

    fix tab active functions

--- vanilaJStabbedComponent/app.js
+++ vanilaJStabbedComponent/app.js
@@ -3,3 +3,18 @@
 const tabBtn = document.querySelectorAll('.tab-item-btn');
 const tabBtnContainer = document.querySelector('.tabbed-items-container');
 const tabContent = document.querySelectorAll('.tab-items-content');
+
+tabBtnContainer.addEventListener('click', (e) => {
+    const cliked = e.target.closest('.tab-item-btn');
+
+    // guard clause
+    if(!cliked) return
+
+    // remove class on not active tab
+    tabBtn.forEach(btn => {
+        btn.classList.remove('tab-active')
+    })
+
+    cliked.classList.add('tab-active');
+
+})
