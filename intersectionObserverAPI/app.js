614e647e5a075f86c67269c3e23c92a0ca0c9aba
Mel <meljohnpualon@gmail.com>
2021-08-19 22:54:27 +0800

    added section reveal function effect

--- intersectionObserverAPI/app.js
+++ intersectionObserverAPI/app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const sections = document.querySelectorAll('.section')
+const revealSections = (entries, observer) => {
+    const [entry] = entries;
+    console.log(entry)
+
+    if(!entry.isIntersecting) return
+    entry.target.classList.add('section-reveal');
+    observer.unobserve(entry.target)
+}
+const sectionObserver = new IntersectionObserver(revealSections, {
+    root: null,
+    threshold: 0.3
+});
+
+sections.forEach(section => {
+    sectionObserver.observe(section);
+    section.classList.remove('section-reveal');
+})
