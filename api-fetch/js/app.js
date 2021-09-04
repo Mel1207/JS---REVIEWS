086d218feaf87cf8a64411f1f6603603ddfb178d
Mel <meljohnpualon@gmail.com>
2021-09-04 10:00:40 +0800

    fetching data from image api

--- api-fetch/js/app.js
+++ api-fetch/js/app.js
@@ -1,3 +1,15 @@
 'use strict'
 
-console.log('hey')
+const fetchAPI = async function (height, width) {
+    try {
+        const getData = await fetch(`https://picsum.photos/id/1/${height}/${width}`);
+        const res = await getData.json();
+
+        return res
+    } catch (err) {
+        throw err
+    }
+  
+}
+
+fetchAPI(100, 200)
