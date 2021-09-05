bd615e9af1c4206cd28c7bb952eb581e6203854f
Mel <meljohnpualon@gmail.com>
2021-09-06 02:32:07 +0800

    transfer json data in dom

--- api-fetch/js/app.js
+++ api-fetch/js/app.js
@@ -2,21 +2,49 @@
 
 const body = document.querySelector('body');
 
-const fetchAPI = async function (height, width) {
+// const fetchAPI = async function (height, width) {
    
-    fetch(`https://picsum.photos/id/1/${height}/${width}`).then(res => {
-        return res
-    }).then(data => {
+//     fetch(`https://picsum.photos/id/1/${height}/${width}`).then(res => {
+//         return res
+//     }).then(data => {
+//         console.log(data)
+
+//         const html = `
+//             <div class="image">
+//                 <img src="${data.url}">
+//             </div>
+//         `
+
+//         body.insertAdjacentHTML('beforeend', html)
+//     })
+// }
+
+// fetchAPI(100, 200)
+
+
+
+
+
+const fetchJson = async function() {
+    fetch('js/blogs.json')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
         console.log(data)
 
-        const html = `
-            <div class="image">
-                <img src="${data.url}">
-            </div>
-        `
+        data.forEach(blog => {
 
-        body.insertAdjacentHTML('beforeend', html)
+            const html = `
+                <h2>Title: ${blog.title} </h2>
+                <h3>Author: ${blog.author}</h3>
+                <h4>Blog</h4>
+                <p>${blog.body}</p>
+            `
+
+            body.insertAdjacentHTML('beforeend', html)
+        })
     })
 }
 
-fetchAPI(100, 200)
+fetchJson()
